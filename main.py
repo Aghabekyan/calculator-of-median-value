8647999006f74a1a7afceae3986d6f78cb72a6d8
Sargis <s.aghabekyan@yandex.ru>
2019-05-11 16:01:08 +0400

    added file reader method

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import math
 
-data = []
+data = [1, 2, 3]
+
+def read_file_to_list(file_name):
+    with open(file_name, "r") as text_file:
+        lines = text_file.readlines()
+        return [int(item) for item in lines]
 
 def list_median_calculator(data):
     length = len(data)
@@ -12,4 +17,5 @@ def list_median_calculator(data):
         min_pos = max_pos - 1
         return float(data[min_pos] + data[max_pos])/2
 
+print(read_file_to_list("data/input00.txt"))
 print(list_median_calculator(data))
