d44a03edce189d3327b930bf96ade03c7c96dfc3
Sargis <s.aghabekyan@yandex.ru>
2019-05-11 16:59:51 +0400

    added main method

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from helpers import read_file_to_list, list_median_calculator
 
-data = read_file_to_list("data/input01.txt")
-count = data.pop(0)
-tmp_list = []
-for i in range(0, count):
-    tmp_list.append(data[i])
-    print(list_median_calculator(sorted(tmp_list)))
+def main():
+    data = read_file_to_list("data/input01.txt")
+    count = data.pop(0)
+    tmp_list = []
+    for i in range(0, count):
+        tmp_list.append(data[i])
+        print(list_median_calculator(sorted(tmp_list)))
+
+
+
+if __name__ == "__main__":
+    main()
