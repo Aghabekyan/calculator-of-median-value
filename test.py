72a5ed5f9cec90ac6916164f401dfff358a35c3a
Sargis <s.aghabekyan@yandex.ru>
2019-05-11 16:57:53 +0400

    minor changes

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ from helpers import read_file_to_list, list_median_calculator
 
 tested_files_dict = {
     "data/input00.txt": "data/output00.txt",
-    "data/input01.txt": "data/output01.txt"
+    "data/input01.txt": "data/output01.txt",
+    "data/input02.txt": "data/output02.txt",
+    "data/input03.txt": "data/output03.txt"
 }
 
 for key in tested_files_dict:
-    print(key)
-    print(tested_files_dict[key])
 
     input_data = read_file_to_list(key)
     output_data = read_file_to_list(tested_files_dict[key], 'float')
@@ -16,5 +16,4 @@ for key in tested_files_dict:
     for i in range(0, count):
         tmp_list.append(input_data[i])
         result = list_median_calculator(sorted(tmp_list))
-        print(result, output_data[i])
-        assert result == output_data[i], "Error"
+        assert result == output_data[i], "Inconsistency in line {0} of output file {1}." .format(i, tested_files_dict[key])
